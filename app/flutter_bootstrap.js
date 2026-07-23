203ba0d04c1bcd8a3664e59b9b59e34573055000
eraycaylak <eraycayylak1@gmail.com>
2026-07-24 02:09:17 +0300

    Mobil 9 ekran referans tasarıma göre yenilendi + kırpma düzeltmeleri

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "720753768" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3042319002" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
